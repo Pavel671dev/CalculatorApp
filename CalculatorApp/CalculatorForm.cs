41745e2e5e1082f77c6283cb32808db75f987843
Pavel671dev <kaedechannyanya@gmail.com>
2025-07-25 16:34:48 +0500

    Fifth iteration

--- CalculatorApp/CalculatorForm.cs
+++ CalculatorApp/CalculatorForm.cs
@@ -1,63 +1,84 @@
+using System.ComponentModel;
+
 namespace CalculatorApp;
 
 public class CalculatorForm : Form
 {
-    private System.ComponentModel.IContainer components = null;
-    
-    private string operation = "";
-    private double resultValue;
+    private Button buttonAstherics;
+    private Button buttonC;
+    private Button buttonCE;
+    private Button buttonDot;
+    private Button buttonEight;
+    private Button buttonEqual;
+    private Button buttonFive;
+    private Button buttonFour;
+    private Button buttonMinus;
+    private Button buttonNine;
+
+    private Button buttonOne;
+    private Button buttonPlus;
+    private Button buttonSeven;
+    private Button buttonSix;
+    private Button buttonSlash;
+    private Button buttonThree;
+    private Button buttonTwo;
+    private Button buttonZero;
+    private IContainer components;
     private bool isPerformed;
 
     private Label labelCurrentOperation;
 
+    private string operation = "";
+    private double resultValue;
+
     private TextBox textBoxResult;
 
-    private Button buttonOne;
-    private Button buttonTwo;
-    private Button buttonThree;
-    private Button buttonFour;
-    private Button buttonFive;
-    private Button buttonSix;
-    private Button buttonSeven;
-    private Button buttonEight;
-    private Button buttonNine;
-    private Button buttonZero;
-    private Button buttonPlus;
-    private Button buttonMinus;
-    private Button buttonSlash;
-    private Button buttonAstherics;
-    private Button buttonDot;
-    private Button buttonEqual;
-    private Button buttonC;
-    private Button buttonCE;
-    
     public CalculatorForm()
     {
         InitializeComponent();
     }
-    
-    public void UnuniqueNumberProperties(Button button)
+
+    public void InitializeComponent()
     {
-        
-        button.Font = new Font("Segoe UI", 12F);
-        button.Size = new Size(72, 76);
-        button.UseVisualStyleBackColor = true;
-        button.Click += ButtonClick;
-        Controls.Add(button);
+        InitializeButtonOne();
+        InitializeButtonTwo();
+        InitializeButtonThree();
+        InitializeButtonFour();
+        InitializeButtonFive();
+        InitializeButtonSix();
+        InitializeButtonSeven();
+        InitializeButtonEight();
+        InitializeButtonNine();
+        InitializeButtonZero();
+        InitializeButtonDot();
+        InitializeButtonPlus();
+        InitializeButtonMinus();
+        InitializeButtonAstherics();
+        InitializeButtonSlash();
+        InitializeButtonEqual();
+        InitializeButtonC();
+        InitializeButtonCE();
+        InitializeBoxResult();
+        InitializeLabelCurrent();
+
+        SuspendLayout();
+
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(409, 449);
+
+        FormBorderStyle = FormBorderStyle.FixedToolWindow;
+        MaximizeBox = false;
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "Calculator";
+        ResumeLayout(false);
+        PerformLayout();
     }
 
-    public void UnuniqueOperationProperties(Button button)
-    {
-        button.Font = new Font("Segoe UI", 12F);
-        button.Size = new Size(72, 76);
-        button.UseVisualStyleBackColor = true;
-        button.Click += OperatorClick;
-        Controls.Add(button);
-    }
     public void InitializeButtonOne()
     {
         buttonOne = new Button();
-        UnuniqueNumberProperties(buttonOne);
+        InitializeCommonNumberProperties(buttonOne);
         buttonOne.Location = new Point(12, 281);
         buttonOne.Name = "buttonOne";
         buttonOne.TabIndex = 9;
@@ -67,17 +88,17 @@ public class CalculatorForm : Form
     public void InitializeButtonTwo()
     {
         buttonTwo = new Button();
-        UnuniqueNumberProperties(buttonTwo);
+        InitializeCommonNumberProperties(buttonTwo);
         buttonTwo.Location = new Point(90, 281);
         buttonTwo.Name = "buttonTwo";
         buttonTwo.TabIndex = 10;
         buttonTwo.Text = "2";
     }
-    
+
     public void InitializeButtonThree()
     {
         buttonThree = new Button();
-        UnuniqueNumberProperties(buttonThree);
+        InitializeCommonNumberProperties(buttonThree);
         buttonThree.Location = new Point(168, 281);
         buttonThree.Name = "buttonThree";
         buttonThree.TabIndex = 11;
@@ -87,16 +108,17 @@ public class CalculatorForm : Form
     public void InitializeButtonFour()
     {
         buttonFour = new Button();
-        UnuniqueNumberProperties(buttonFour);
+        InitializeCommonNumberProperties(buttonFour);
         buttonFour.Location = new Point(12, 199);
         buttonFour.Name = "buttonFour";
         buttonFour.TabIndex = 5;
         buttonFour.Text = "4";
     }
+
     public void InitializeButtonFive()
     {
         buttonFive = new Button();
-        UnuniqueNumberProperties(buttonFive);
+        InitializeCommonNumberProperties(buttonFive);
         buttonFive.Location = new Point(90, 199);
         buttonFive.Name = "buttonFive";
         buttonFive.TabIndex = 6;
@@ -106,7 +128,7 @@ public class CalculatorForm : Form
     public void InitializeButtonSix()
     {
         buttonSix = new Button();
-        UnuniqueNumberProperties(buttonSix);
+        InitializeCommonNumberProperties(buttonSix);
         buttonSix.Location = new Point(168, 199);
         buttonSix.Name = "buttonSix";
         buttonSix.TabIndex = 6;
@@ -116,7 +138,7 @@ public class CalculatorForm : Form
     public void InitializeButtonSeven()
     {
         buttonSeven = new Button();
-        UnuniqueNumberProperties(buttonSeven);
+        InitializeCommonNumberProperties(buttonSeven);
         buttonSeven.Location = new Point(12, 117);
         buttonSeven.Name = "buttonSeven";
         buttonSeven.TabIndex = 0;
@@ -126,7 +148,7 @@ public class CalculatorForm : Form
     public void InitializeButtonEight()
     {
         buttonEight = new Button();
-        UnuniqueNumberProperties(buttonEight);
+        InitializeCommonNumberProperties(buttonEight);
         buttonEight.Location = new Point(90, 117);
         buttonEight.Name = "buttonEight";
         buttonEight.TabIndex = 2;
@@ -136,7 +158,7 @@ public class CalculatorForm : Form
     public void InitializeButtonNine()
     {
         buttonNine = new Button();
-        UnuniqueNumberProperties(buttonNine);
+        InitializeCommonNumberProperties(buttonNine);
         buttonNine.Location = new Point(168, 117);
         buttonNine.Name = "buttonNine";
         buttonNine.TabIndex = 3;
@@ -146,7 +168,7 @@ public class CalculatorForm : Form
     public void InitializeButtonZero()
     {
         buttonZero = new Button();
-        UnuniqueNumberProperties(buttonZero);
+        InitializeCommonNumberProperties(buttonZero);
         buttonZero.Size = new Size(150, 76);
         buttonZero.Location = new Point(12, 363);
         buttonZero.Name = "buttonZero";
@@ -157,17 +179,26 @@ public class CalculatorForm : Form
     public void InitializeButtonDot()
     {
         buttonDot = new Button();
-        UnuniqueNumberProperties(buttonDot);
+        InitializeCommonNumberProperties(buttonDot);
         buttonDot.Location = new Point(168, 363);
         buttonDot.Name = "buttonDot";
         buttonDot.TabIndex = 17;
         buttonDot.Text = ",";
     }
 
+    public void InitializeCommonNumberProperties(Button button)
+    {
+        button.Font = new Font("Segoe UI", 12F);
+        button.Size = new Size(72, 76);
+        button.UseVisualStyleBackColor = true;
+        button.Click += ButtonClick;
+        Controls.Add(button);
+    }
+
     public void InitializeButtonPlus()
     {
         buttonPlus = new Button();
-        UnuniqueOperationProperties(buttonPlus);
+        InitializeCommonOperationProperties(buttonPlus);
         buttonPlus.Location = new Point(246, 361);
         buttonPlus.Name = "buttonPlus";
         buttonPlus.TabIndex = 18;
@@ -177,7 +208,7 @@ public class CalculatorForm : Form
     public void InitializeButtonMinus()
     {
         buttonMinus = new Button();
-        UnuniqueOperationProperties(buttonMinus);
+        InitializeCommonOperationProperties(buttonMinus);
         buttonMinus.Location = new Point(246, 281);
         buttonMinus.Name = "buttonMinus";
         buttonMinus.TabIndex = 12;
@@ -187,7 +218,7 @@ public class CalculatorForm : Form
     public void InitializeButtonAstherics()
     {
         buttonAstherics = new Button();
-        UnuniqueOperationProperties(buttonAstherics);
+        InitializeCommonOperationProperties(buttonAstherics);
         buttonAstherics.Location = new Point(246, 117);
         buttonAstherics.Name = "buttonAstherics";
         buttonAstherics.TabIndex = 4;
@@ -197,11 +228,20 @@ public class CalculatorForm : Form
     public void InitializeButtonSlash()
     {
         buttonSlash = new Button();
-        UnuniqueOperationProperties(buttonSlash);
+        InitializeCommonOperationProperties(buttonSlash);
         buttonSlash.Location = new Point(246, 199);
         buttonSlash.Name = "buttonSlash";
         buttonSlash.TabIndex = 8;
         buttonSlash.Text = "/";
+    }
+
+    public void InitializeCommonOperationProperties(Button button)
+    {
+        button.Font = new Font("Segoe UI", 12F);
+        button.Size = new Size(72, 76);
+        button.UseVisualStyleBackColor = true;
+        button.Click += OperatorClick;
+        Controls.Add(button);
     }
 
     public void InitializeButtonEqual()
@@ -269,63 +309,25 @@ public class CalculatorForm : Form
         labelCurrentOperation.TabIndex = 21;
         Controls.Add(labelCurrentOperation);
     }
-    public void InitializeComponent()
-    {
-        InitializeButtonOne();
-        InitializeButtonTwo();
-        InitializeButtonThree();
-        InitializeButtonFour();
-        InitializeButtonFive();
-        InitializeButtonSix();
-        InitializeButtonSeven();
-        InitializeButtonEight();
-        InitializeButtonNine();
-        InitializeButtonZero();
-        InitializeButtonDot();
-        InitializeButtonPlus();
-        InitializeButtonMinus();
-        InitializeButtonAstherics();
-        InitializeButtonSlash();
-        InitializeButtonEqual();
-        InitializeButtonC();
-        InitializeButtonCE();
-        InitializeBoxResult();
-        InitializeLabelCurrent();
 
-        SuspendLayout();
-
-        AutoScaleDimensions = new SizeF(8F, 20F);
-        AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(409, 449);
-        
-        FormBorderStyle = FormBorderStyle.FixedToolWindow;
-        MaximizeBox = false;
-        StartPosition = FormStartPosition.CenterScreen;
-        Text = "Calculator";
-        ResumeLayout(false);
-        PerformLayout();
-    }
     protected override void Dispose(bool disposing)
     {
-        if (disposing && (components != null))
-        {
-            components.Dispose();
-        }
+        if (disposing && components != null) components.Dispose();
 
         base.Dispose(disposing);
     }
+
     private void ButtonClick(object sender, EventArgs e)
     {
         // Method is used for 0123456789 and , buttons
         isPerformed = false;
-        Button button = (Button)sender;
+        var button = (Button)sender;
         if (textBoxResult.Text == "Error") textBoxResult.Text = "0";
-        if ((textBoxResult.Text == "0") && (button.Text != ","))
+        if (textBoxResult.Text == "0" && button.Text != ",")
             textBoxResult.Clear();
         // Separator is not added if it already exists in number
         if (button.Text != "," || !textBoxResult.Text.Contains(","))
             textBoxResult.Text = textBoxResult.Text + button.Text;
-
     }
 
     private void OperatorClick(object sender, EventArgs e)
@@ -335,11 +337,11 @@ public class CalculatorForm : Form
             textBoxResult.Text = "0";
         if (isPerformed)
             return;
-        Button button = (Button)sender;
+        var button = (Button)sender;
         operation = button.Text;
         resultValue = double.Parse(textBoxResult.Text);
         // First typed number and operation will be displayed under the line with result
-        labelCurrentOperation.Text = (textBoxResult.Text + operation);
+        labelCurrentOperation.Text = textBoxResult.Text + operation;
         isPerformed = true;
         textBoxResult.Text = "0";
     }
@@ -357,42 +359,46 @@ public class CalculatorForm : Form
         textBoxResult.Text = "0";
     }
 
-    private string HandleClickPlus()
-    {
-        return (resultValue + double.Parse(textBoxResult.Text)).ToString();
-    }
-    
-    private string HandleClickMinus()
-    {
-        return (resultValue - double.Parse(textBoxResult.Text)).ToString();
-    }
-    private string HandleClickAstherics()
-    {
-        return (resultValue * double.Parse(textBoxResult.Text)).ToString();
-    }
-    private string HandleClickDivide()
-    {
-        return (double.Parse(textBoxResult.Text) == 0) ? "Error" : (resultValue / double.Parse(textBoxResult.Text)).ToString();
-    }
-    
     private void EqualClick(object sender, EventArgs e)
     {
         switch (operation)
         {
             case "+":
-                textBoxResult.Text = HandleClickPlus();
+                textBoxResult.Text = CalculateSum();
                 break;
             case "-":
-                textBoxResult.Text = HandleClickMinus();
+                textBoxResult.Text = CalculateDifference();
                 break;
             case "*":
-                textBoxResult.Text = HandleClickAstherics();
+                textBoxResult.Text = CalculateProduct();
                 break;
             case "/":
-                textBoxResult.Text = HandleClickDivide();
+                textBoxResult.Text = CalculateQuotient();
                 break;
             default:
                 return;
         }
+    }
+
+    private string CalculateSum()
+    {
+        return (resultValue + double.Parse(textBoxResult.Text)).ToString();
+    }
+
+    private string CalculateDifference()
+    {
+        return (resultValue - double.Parse(textBoxResult.Text)).ToString();
+    }
+
+    private string CalculateProduct()
+    {
+        return (resultValue * double.Parse(textBoxResult.Text)).ToString();
+    }
+
+    private string CalculateQuotient()
+    {
+        return textBoxResult.Text == "0"
+            ? "Error"
+            : (resultValue / double.Parse(textBoxResult.Text)).ToString();
     }
 }
